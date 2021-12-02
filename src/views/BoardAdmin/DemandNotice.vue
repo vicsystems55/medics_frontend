a18fa23829c9dee22cef40dcd911b0677e1213f6
vicSystems <victorasuquob@gmail.com>
2021-12-02 10:27:39 +0100

    state logo

--- src/views/BoardAdmin/DemandNotice.vue
+++ src/views/BoardAdmin/DemandNotice.vue
@@ -42,60 +42,70 @@
                 </div>
     
         <div id="printMe" style="min-height: 312px; width: 730px;" class="container shadow bg-white py-5">
-            <h4 class="text-center">KADUNA STATE INTERNAL REVENUE SERVICE</h4>
-            <h6 class="text-center">HEAD OFFICE: OBASANJO HOUSE, YAKUBU GOWON WAY, KADUNA</h6>
+        <div class="container">
+                <div class="row">
+                <div class="col-3">
+                    <img src="state_logo/kaduna.jpg" style="height: 120px;" alt="">
+                </div>
+                <div class="col-9 pt-3">
+                    <h4 class="text-cente">KADUNA STATE INTERNAL REVENUE SERVICE</h4>
+                    <h6 class="text-centr">HEAD OFFICE: OBASANJO HOUSE, YAKUBU GOWON WAY, KADUNA</h6>
+                </div>
+            </div>
+        </div>
+   
             <div class="container">
                 <table class="table">
-                 <tr class="table__row">
-                    <td class="table__td">Name</td>
-                    <td class="table__td">{{businessName}}</td>
-                    <td class="table__td">TIN</td>
-                    <td class="table__td"></td>
+                 <tr class="">
+                    <td class=" "><span class="font-weight-bold">Name</span></td>
+                    <td class=" ">{{businessName}}</td>
+                    <td class=" "><span class="font-weight">TIN</span></td>
+                    <td class=" "></td>
+                </tr>
+
+                     <tr class="">
+                    <td class=" "><span class="font-weight-bold">Address</span></td>
+                    <td class=" ">{{businessAddress}}</td>
+                    <td class=" "><span class="font-weight-bold">Assesment No.</span></td>
+                    <td class=" "></td>
                 </tr>
 
                      <tr class="table__row">
-                    <td class="table__td">Address</td>
-                    <td class="table__td">{{businessAddress}}</td>
-                    <td class="table__td">Assesment No.</td>
-                    <td class="table__td"></td>
-                </tr>
-
-                     <tr class="table__row">
-                    <td class="table__td">Phone</td>
-                    <td class="table__td">{{businessPhoneNo}}</td>
-                    <td class="table__td">Date:</td>
-                    <td class="table__td"></td>
+                    <td class="table__td "><span class="font-weight-bold">Phone</span></td>
+                    <td class="table__td ">{{businessPhoneNo}}</td>
+                    <td class="table__td "><span class="font-weight-bold">Date:</span></td>
+                    <td class="table__td "></td>
                 </tr>
             </table>
             </div>
-        <p class="px-5">
+        <p class=" px-5">
             In pursuance of Section 7(4th Schedule of the 1999 constitution), Section 98 and Section 99 (a, b, and c) of the Kaduna State Tax (codification and Consolidation) Law, 2020 which impose a tax on ownership of device or equipment capable of receiving and transmitting   radio and television broadcast content in the state. A notice of assessment is hereby served on you in respect of the device(s) usage for the year(s) stated below;
         </p>
         <div class="container">
             <h6 class="px-5">RADIO</h6>
-                <table class="table table--lines">
+                <table class="table ">
                  <tr class="table__row">
-                    <td class="table__td">ASSESSMENT PERIOD</td>
-                    <td class="table__td">2020</td>
-                    <td class="table__td">2021</td>
+                    <td class="table__td border">ASSESSMENT PERIOD</td>
+                    <td class="table__td border">2020</td>
+                    <td class="table__td border">2021</td>
                 </tr>
 
                  <tr class="table__row">
-                    <td class="table__td">Number of Devices</td>
-                    <td class="table__td">92</td>
-                    <td class="table__td">92</td>
+                    <td class="table__td border">Number of Devices</td>
+                    <td class="table__td border">92</td>
+                    <td class="table__td border">92</td>
                 </tr>
 
                  <tr class="table__row">
-                    <td class="table__td">Annual Rate per Device</td>
-                    <td class="table__td">N3,600 </td>
-                    <td class="table__td">N3,600 </td>
+                    <td class="table__td border">Annual Rate per Device</td>
+                    <td class="table__td border">N3,600 </td>
+                    <td class="table__td border">N3,600 </td>
                 </tr>
 
                  <tr class="table__row">
-                    <td class="table__td"></td>
-                    <td class="table__td"></td>
-                    <td class="table__td"></td>
+                    <td class="table__td border"></td>
+                    <td class="table__td border"></td>
+                    <td class="table__td border"></td>
                 </tr>
             </table>
         </div>
@@ -104,29 +114,29 @@
                  <h6 class="px-5">TELEVISION</h6>
                 <table class="table table--lines">
                  <tr class="table__row">
-                    <td class="table__td">ASSESSMENT PERIOD</td>
-                    <td class="table__td">2020</td>
-                    <td class="table__td">2021</td>
+                    <td class="table__td border">ASSESSMENT PERIOD</td>
+                    <td class="table__td border">2020</td>
+                    <td class="table__td border">2021</td>
                 </tr>
 
           
 
                  <tr class="table__row">
-                    <td class="table__td">Number of Devices</td>
-                    <td class="table__td">92</td>
-                    <td class="table__td">92</td>
+                    <td class="table__td border">Number of Devices</td>
+                    <td class="table__td border">92</td>
+                    <td class="table__td border">92</td>
                 </tr>
 
                  <tr class="table__row">
-                    <td class="table__td">Annual Rate per Device</td>
-                    <td class="table__td">N3,600 </td>
-                    <td class="table__td">N3,600 </td>
+                    <td class="table__td border">Annual Rate per Device</td>
+                    <td class="table__td border">N3,600 </td>
+                    <td class="table__td border">N3,600 </td>
                 </tr>
 
                  <tr class="table__row">
-                    <td class="table__td"></td>
-                    <td class="table__td"></td>
-                    <td class="table__td"></td>
+                    <td class="table__td border"></td>
+                    <td class="table__td border"></td>
+                    <td class="table__td border"></td>
                 </tr>
             </table>
         </div>
@@ -135,28 +145,28 @@
                  <h6 class="px-5">COMPUTERS</h6>
                 <table class="table table--lines">
                  <tr class="table__row">
-                    <td class="table__td">ASSESSMENT PERIOD</td>
-                    <td class="table__td">2020</td>
-                    <td class="table__td">2021</td>
+                    <td class="table__td border">ASSESSMENT PERIOD</td>
+                    <td class="table__td border">2020</td>
+                    <td class="table__td border">2021</td>
                 </tr>
 
 
                  <tr class="table__row">
-                    <td class="table__td">Number of Devices</td>
-                    <td class="table__td">92</td>
-                    <td class="table__td">92</td>
+                    <td class="table__td border">Number of Devices</td>
+                    <td class="table__td border">92</td>
+                    <td class="table__td border">92</td>
                 </tr>
 
                  <tr class="table__row">
-                    <td class="table__td">Annual Rate per Device</td>
-                    <td class="table__td">N3,600 </td>
-                    <td class="table__td">N3,600 </td>
+                    <td class="table__td border">Annual Rate per Device</td>
+                    <td class="table__td border">N3,600 </td>
+                    <td class="table__td border">N3,600 </td>
                 </tr>
 
                  <tr class="table__row">
-                    <td class="table__td"></td>
-                    <td class="table__td"></td>
-                    <td class="table__td"></td>
+                    <td class="table__td border"></td>
+                    <td class="table__td border"></td>
+                    <td class="table__td border"></td>
                 </tr>
             </table>
         </div>
@@ -216,7 +226,7 @@ export default {
                 this.businessName = response.data.businessDetails.businessName
                 this.businessAddress = response.data.businessDetails.businessAddress
                 this.businessPhoneNo = response.data.businessDetails.businessPhoneNo
-                alert('demand details')
+                // alert('demand details')
                 console.log(response.data.demandNoticeDetails)
                 //  console.log(this.demandNoticeDetails)
                 loader.hide()
